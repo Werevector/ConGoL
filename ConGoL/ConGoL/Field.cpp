6bdc116b497a96a8687a286e384f4f66a8581b07
Werevector <runartistel@gmail.com>
2015-02-10 13:27:01 +0100

    Field

--- ConGoL/ConGoL/Field.cpp
+++ ConGoL/ConGoL/Field.cpp
@@ -1,2 +1,15 @@
 #include "Field.h"
 
+//Innitiate Field Default
+void Field::Innitiate_Field(){
+
+}
+
+//Innitiate Field with Premade
+void Field::Innitiate_Field_PRE(){
+
+}
+
+void Field::Set_State_DEAD(int x, int y){
+	 
+}
--- ConGoL/ConGoL/Field.h
+++ ConGoL/ConGoL/Field.h
@@ -22,7 +22,7 @@ public:
 
 private:
 
-	std::vector<int> l_Field;
+	std::vector<std::vector<int>> l_Field;
 
 
 };

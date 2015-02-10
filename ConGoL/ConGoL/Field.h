92a81bc28eed09f711069ca82caab49898d966e6
Werevector <runartistel@gmail.com>
2015-02-10 09:50:09 -0800

    extra files

---
+++ ConGoL/ConGoL/Field.h
@@ -0,0 +1,28 @@
+#include <vector>;
+
+class Field{
+
+public:
+
+	Field();
+
+	//Innitiate Field Default or Premade 
+	void Innitiate_Field();
+	void Innitiate_Field_PRE();
+
+	//Return reference to Field
+	std::vector<int>& get_FieldRef();
+
+	//Set a single space(x,y) to DEAD or LIVE
+	void Set_State_DEAD(int x, int y);
+	void Set_State_LIVE(int x, int y);
+
+
+
+
+private:
+
+	std::vector<int> l_Field;
+
+
+};
--- ConGoL/ConGoL/Main.cpp
+++ ConGoL/ConGoL/Main.cpp
@@ -1,9 +1,43 @@
+#pragma once
 #include <iostream>
+#include "SDL.h"
 
-int main(){
+void close();
 
-	std::cout << "Hello World\n";
+int main(int argc, char* args[]) {
 
-	system("pause");
+	//initialize the game
+	
+	bool quit = false;
+	SDL_Event event;
 
+
+	while (!quit)
+	{
+		while (SDL_PollEvent(&event) != 0){
+			if (event.type == SDL_QUIT){
+				quit = true;
+			}
+		}//EventWhile
+
+		const Uint8* currentKeyStates = SDL_GetKeyboardState(NULL);
+
+	}//GameLoop
+
+	close();
+
+	return 0;
+
+}//main
+
+void close()
+{
+	//Destroy window	
+	/*SDL_DestroyRenderer(Graphics::gRenderer);
+	SDL_DestroyWindow(Graphics::gWindow);
+	Graphics::gWindow = NULL;
+	Graphics::gRenderer = NULL*/;
+
+	//Quit SDL subsystems
+	SDL_Quit();
 }
